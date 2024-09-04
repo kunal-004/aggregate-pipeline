e3529a2d7fbda016b84bc633ff8b4fc2190ca5ae
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-09-04 22:40:00 +0530

    api error class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-const app = express();
 dotenv.config({ path: "./env" });
+const PORT = process.env.PORT || 8000;
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    app.on("error", (err) => {
+      console.log(`Server error ${err}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo Connection failed", err);
+    process.exit(1);
+  });
 
 // (async () => {
 //   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.send(error.code || 500).json({
+//       status: false
+//       message: error.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
