92fd626f0e4d423a5591332a093df750eb95bd03
Kunal Pitale <kunal.212034105@vcet.edu.in>
2024-09-05 08:16:04 +0530

    cloudinary config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const CloudinaryFileUpload = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    const Response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("Fole uploaded", Response.url);
+    return Response;
+  } catch (error) {
+    console.error("Error uploading file", error);
+    fs.unlinkSync(localFilePath); //remove temp file from server as ops fails
+  }
+};
